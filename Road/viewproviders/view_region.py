572a3bc8136ef2c49d81e6b5e62dbe59cf34c44a
HakanSeven12 <hakanseven12@gmail.com>
2025-12-13 14:26:39 +0300

    alignment and region label manager integration

--- Road/viewproviders/view_region.py
+++ Road/viewproviders/view_region.py
@@ -5,8 +5,9 @@
 import FreeCAD
 from pivy import coin
 import math
-from ..geometry.first_iteration.alignment_old import transformation
 from .view_geo_object import ViewProviderGeoObject
+from ..utils.label_manager import LabelManager
+from ..utils.support import  zero_referance
 
 
 class ViewProviderRegion(ViewProviderGeoObject):
@@ -35,18 +36,11 @@ class ViewProviderRegion(ViewProviderGeoObject):
             "Justification of station label").Justification = ["Left", "Center", "Right"]
 
         vobj.addProperty(
-            "App::PropertyFloat", "OffsetX", "Label Placement",
-            "Horizontal offset between station label and tick").OffsetX = 1
-
-        vobj.addProperty(
-            "App::PropertyFloat", "OffsetY", "Label Placement",
-            "Vertical offset between point station and tick").OffsetY = 0
-
-        vobj.addProperty(
-            "App::PropertyVector", "LabelOffset", "Label Placement",
-            "Points of reference for station label").LabelOffset = FreeCAD.Vector(vobj.OffsetX, vobj.OffsetY)
+            "App::PropertyPlacement", "Transformation", "Label Placement",
+            "Placement").Transformation = FreeCAD.Placement()
 
         vobj.Proxy = self
+        self.label_manager = None
 
     def attach(self, vobj):
         """Create Object visuals in 3D view."""
@@ -86,16 +80,8 @@ class ViewProviderRegion(ViewProviderGeoObject):
         # Labels
         #-----------------------------------------------------------------
 
-        # Text
-        self.font = coin.SoFont()
-        self.label_color = coin.SoBaseColor()
-
-        self.texts = coin.SoGroup()
-
-        self.labels = coin.SoSeparator()
-        self.labels.addChild(self.font)
-        self.labels.addChild(self.label_color)
-        self.labels.addChild(self.texts)
+        labels_root = coin.SoSeparator()
+        self.label_manager = LabelManager(labels_root)
 
         #-----------------------------------------------------------------
         # Region
@@ -105,7 +91,7 @@ class ViewProviderRegion(ViewProviderGeoObject):
         standard_selection = coin.SoType.fromName('SoFCSelection').createInstance()
         standard_selection.style = 'EMISSIVE_DIFFUSE'
         standard_selection.addChild(lines)
-        standard_selection.addChild(self.labels)
+        standard_selection.addChild(labels_root)
 
         self.standard.addChild(standard_selection)
 
@@ -121,78 +107,72 @@ class ViewProviderRegion(ViewProviderGeoObject):
     def onChanged(self, vobj, prop):
         """Update Object visuals when a view property changed."""
         if prop == "Labels":
-            self.texts.removeAllChildren()
+            if not hasattr(self, 'label_manager') or self.label_manager is None:
+                return
+            if not hasattr(vobj, 'Transformation'):
+                return
+                
+            self.label_manager.clear_labels()
+            
+            if not vobj.Labels:
+                return
+                
             regions = vobj.Object.getParentGroup()
             alignment = regions.getParentGroup()
 
-            tangent = vobj.Object.IncrementAlongTangents
-            curve = vobj.Object.IncrementAlongCurves
-            spiral = vobj.Object.IncrementAlongSpirals
-            horizontal = vobj.Object.AtHorizontalGeometry
+            for station in vobj.Object.Stations:
+                # Get point and direction from alignment
+                tuple_coord, tuple_vec = alignment.Model.get_orthogonal_at_station(station, "left")
+                coord = zero_referance(alignment.Model.get_start_point(), [tuple_coord])
+                point = coord[0].add(alignment.Placement.Base)
+                dir_vec = FreeCAD.Vector(*tuple_vec)
+                
+                # Calculate angle from direction vector
+                angle = math.atan2(dir_vec.y, dir_vec.x)
+                
+                # Prepare placement and rotation
+                placement = FreeCAD.Placement()
+                placement.Base = FreeCAD.Vector(0, 0, 0)
+                placement.Rotation = FreeCAD.Rotation(FreeCAD.Vector(0, 0, 1), math.degrees(angle))
 
-            stations = transformation(alignment, tangent, curve, spiral, horizontal)
-            for station, transform in stations.items():
-                point = transform["Location"]
-                angle = transform["Rotation"]
-
-                transform = coin.SoTransform()
-                location = coin.SoTranslation()
-                text = coin.SoAsciiText()
-
-
-                if vobj.Justification == "Left":
-                    text.justification = coin.SoAsciiText.LEFT
-                elif vobj.Justification == "Right":
-                    text.justification = coin.SoAsciiText.RIGHT
-                else:
-                    text.justification = coin.SoAsciiText.CENTER
-
-                if math.pi / 2 < angle < 3 * math.pi /2:
-                    text.justification = 2 if text.justification == 1 else 1
-                    angle = (angle + math.pi) % (2 * math.pi)
-
-                transform.translation = point
-                transform.rotation.setValue(coin.SbVec3f(0, 0, 1), angle)
-                location.translation = vobj.LabelOffset 
-
-                station = str(station).zfill(6)
-                integer = station.split('.')[0]
+                # Format station text
+                station_str = str(station).zfill(6)
+                integer = station_str.split('.')[0]
                 new_integer = integer[:-3] + "+" + integer[-3:]
-
-                text.string.setValues([new_integer + "." + station.split('.')[1]])
-
-                group = coin.SoTransformSeparator()
-                group.addChild(transform)
-                group.addChild(location)
-                group.addChild(text)
-                self.texts.addChild(group)
-
+                text = new_integer + "." + station_str.split('.')[1]
+                
+                self.label_manager.add_label(
+                    position=point,
+                    text=text,
+                    side=vobj.Justification,
+                    spacing=1.0,
+                    transformation=placement.multiply(vobj.Transformation)
+                )
 
         elif prop == "LabelColor":
-            color = vobj.getPropertyByName(prop)
-            self.label_color.rgb = (color[0], color[1], color[2])
+            if self.label_manager:
+                color = vobj.getPropertyByName(prop)
+                self.label_manager.material.diffuseColor = (color[0], color[1], color[2])
 
         elif prop == "LabelSize":
-            self.font.size = vobj.getPropertyByName(prop) * 1000
-            self.onChanged(vobj, "OffsetY")
+            if self.label_manager:
+                self.label_manager.font.size = vobj.getPropertyByName(prop) * 1000
+            self.onChanged(vobj, "Labels")
 
         elif prop == "FontName":
-            self.font.name = vobj.getPropertyByName(prop).encode("utf8")
+            if self.label_manager:
+                self.label_manager.font.name = vobj.getPropertyByName(prop).encode("utf8")
 
         elif prop == "Justification":
-            self.onChanged(vobj, "OffsetX")
+            self.onChanged(vobj, "Labels")
+
+        elif prop == "Transformation":
+            self.onChanged(vobj, "Labels")
 
         elif prop == "OffsetX":
-            justification = -1 if vobj.Justification == "Right" else 1
-            vobj.LabelOffset.x = vobj.getPropertyByName(prop) * 1000 * justification
-            self.onChanged(vobj, "LabelOffset")
+            self.onChanged(vobj, "Labels")
 
         elif prop == "OffsetY":
-            y_offset = vobj.getPropertyByName(prop)
-            vobj.LabelOffset.y = -vobj.LabelSize / 2 + y_offset * 1000
-            self.onChanged(vobj, "LabelOffset")
-
-        elif prop == "LabelOffset":
             self.onChanged(vobj, "Labels")
 
     def updateData(self, obj, prop):
