3949a0b50c34704f9c3c9a63f7ccd6ffd8596fe9
HakanSeven12 <hakanseven12@gmail.com>
2026-02-11 22:50:05 +0300

    road create ui changes

--- Road/guitools/gui_road_create.py
+++ Road/guitools/gui_road_create.py
@@ -6,7 +6,7 @@ import FreeCAD, FreeCADGui
 
 from ..variables import icons_path
 from ..make import make_road
-from ..tasks.task_selection import SingleSelection
+from ..tasks.task_selection import SingleSelection, SimpleComboBox
 
 
 class RoadCreate:
@@ -31,46 +31,36 @@ class RoadCreate:
     def Activated(self):
         """Command activation method"""
         alignments = FreeCAD.ActiveDocument.getObject("Alignments")
-        self.alignment_selector = SingleSelection(alignments)
-        self.alignment_selector.combo_box.currentTextChanged.connect(self.frame_update)
-
-        self.frame_selector = SingleSelection()
-        self.frame_selector.combo_box.currentTextChanged.connect(self.profile_update)
-
-        self.profile_selector = SingleSelection()
-        self.frame_update()
-        self.profile_update()
-
         structures = FreeCAD.ActiveDocument.getObject("Structures")
+
+        self.alignment_selector = SingleSelection(alignments)
+        self.profile_selector = SimpleComboBox(title="Select Profile")
         self.structure_selector = SingleSelection(structures)
 
-        self.form = [self.alignment_selector, self.frame_selector, self.profile_selector, self.structure_selector]
+        self.alignment_selector.combo_box.currentTextChanged.connect(self.profile_update)
+        self.profile_update()
+
+        self.form = [self.alignment_selector, self.profile_selector, self.structure_selector]
         FreeCADGui.Control.showDialog(self)
 
-    def frame_update(self):
-        alignment = self.alignment_selector.selected_object
-
-        for item in alignment.Group:
-            if item.Proxy.Type == "Road::Profiles":
-                self.frame_selector.update_items(item)
-
     def profile_update(self):
-        profile_frame = self.frame_selector.selected_object
-        self.profile_selector.update_items(profile_frame)
+        """Update profile selector when alignment changes"""
+        alignment = self.alignment_selector.selected_object
+        profiles = alignment.Model.profiles
+        profile_names = profiles.get_profalign_names()
+        self.profile_selector.update_items(profile_names)
 
     def accept(self):
         """Panel 'OK' button clicked"""
         alignment = self.alignment_selector.selected_object
-        profile = self.profile_selector.selected_object
+        selected_profile_name = self.profile_selector.selected_item
         structure = self.structure_selector.selected_object
 
         road = make_road.create()
-        road.Alignment = alignment
-        road.Profile = profile
-        road.Structure = structure
 
-        roads = FreeCAD.ActiveDocument.getObject("Roads")
-        roads.addObject(road)
+        road.Alignment = alignment
+        road.Profile = selected_profile_name
+        road.Structure = structure
 
         FreeCAD.ActiveDocument.recompute()
         FreeCADGui.Control.closeDialog()
