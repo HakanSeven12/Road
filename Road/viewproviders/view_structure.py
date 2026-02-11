bc8a31ecb471fba4925d448d113bf07768b9544f
HakanSeven12 <hakanseven12@gmail.com>
2026-02-11 17:00:32 +0300

    structure view fix

--- Road/viewproviders/view_structure.py
+++ Road/viewproviders/view_structure.py
@@ -5,19 +5,22 @@
 import FreeCAD
 from pivy import coin
 
+from .view_geo_object import ViewProviderGeoObject
 from ..variables import icons_path
 from ..utils.get_group import create_project
 
 
-class ViewProviderStructure:
+class ViewProviderStructure(ViewProviderGeoObject):
     """This class is about Profile Object view features."""
     def __init__(self, vobj):
         """Set view properties."""
-
+        super().__init__(vobj, "Structure")
         vobj.Proxy = self
 
     def attach(self, vobj):
         """Create Object visuals in 3D view."""
+        super().attach(vobj)
+
         self.Object = vobj.Object
 
         self.draw_style = coin.SoDrawStyle()
@@ -58,28 +61,16 @@ class ViewProviderStructure:
         structure_selection.style = 'EMISSIVE_DIFFUSE'
         structure_selection.addChild(lines)
 
-        self.structure = coin.SoGeoSeparator()
-        self.structure.addChild(structure_selection)
-
-        vobj.addDisplayMode(self.structure, "Flat Lines")
+        self.standard.addChild(structure_selection)
 
     def updateData(self, obj, prop):
         """Update Object visuals when a data property changed."""
-        if prop == "Placement":
-            placement = obj.getPropertyByName(prop)
-            origin = create_project()
-            geo_system = ["UTM", origin.UtmZone, "FLAT"]
+        super().updateData(obj, prop)
 
-            self.structure.geoSystem.setValues(geo_system)
-            self.structure.geoCoords.setValue(placement.Base.x, placement.Base.y, placement.Base.z)
-
-        elif prop == "Shape":
-            shape = obj.getPropertyByName(prop).copy()
-            shape.Placement.move(obj.Placement.Base.negative())
-
+        if prop == "Shape":
             line_coords = []
             line_index = []
-            for edge in shape.Edges:
+            for edge in obj.Shape.Edges:
                 start = len(line_coords)
                 line_coords.extend(edge.discretize(2))
                 end = len(line_coords)
@@ -90,32 +81,6 @@ class ViewProviderStructure:
             self.line_coords.point.values = line_coords
             self.line_lines.coordIndex.values = line_index
 
-    def getDisplayModes(self,vobj):
-        """Return a list of display modes."""
-        modes = ["Flat Lines"]
-        return modes
-
-    def getDefaultDisplayMode(self):
-        """Return the name of the default display mode."""
-        return "Flat Lines"
-
-    def setDisplayMode(self,mode):
-        """Map the display mode defined in attach with 
-        those defined in getDisplayModes."""
-        return mode
-
-    def getIcon(self):
-        """Return object treeview icon."""
-        return icons_path + "/Structure.svg"
-
     def claimChildren(self):
         """Provides object grouping"""
         return self.Object.Group
-
-    def dumps(self):
-        """Called during document saving"""
-        return None
-
-    def loads(self, state):
-        """Called during document restore."""
-        return None
